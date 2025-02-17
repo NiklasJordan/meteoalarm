42f91ea784ece7802f6821d59797fd7dafdc998c
Niklas Jordan <hello@niklasjordan.com>
2025-02-17 13:32:27 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -7,27 +7,9 @@ author = 'Niklas Jordan'
 
 # -- General configuration ---------------------------------------------------
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx_inline_tabs',
-    'sphinx_examples',
-    'sphinx_proof',
-    'sphinx_hoverxref',
-]
-
-templates_path = ['_templates']
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
-
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_documents = [
-    ('index', 'meteoalarm.tex', 'MeteoAlarm library documentation',
-     'Niklas Jordan', 'manual'),
-]
